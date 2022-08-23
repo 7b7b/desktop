ec28a0659f10214b63cba7b4c9bbc8198b5ab507
Git-Fal7 <th1s.1s.r3dm2cs@gmail.com>
2022-08-22 22:16:43 -0400

    hover

--- panel/panel-plugins/LTBWidget.h
+++ panel/panel-plugins/LTBWidget.h
@@ -21,15 +21,13 @@ private:
 	LXCB::WINDOWVISIBILITY cstate;
 	QString rawstyle;
 	void updateBackground(){
-
 	  QString background = "background: transparent; "; //default value
 	  QString border = "border: 1px solid transparent;";
-
 	  if(cstate == LXCB::IGNORE){  this->setObjectName(""); } //just use the defaults
 	  else if(cstate == LXCB::VISIBLE){ this->setObjectName("WindowVisible"); }//background = "background: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(255, 255, 255, 240), stop:0.505682 rgba(240, 240, 240, 150), stop:1 rgba(210, 210, 210, 55));"; border="border: 1px solid transparent;"; }
 	  else if(cstate == LXCB::INVISIBLE){this->setObjectName("WindowInvisible"); } //background = "background: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(215, 215, 215, 240), stop:0.505682 rgba(184, 185, 186, 150), stop:1 rgba(221, 246, 255, 55));"; border="border: 1px solid transparent;"; }
 	  else if(cstate == LXCB::ACTIVE){ this->setObjectName("WindowActive"); }//background= "background: qlineargradient(spkground: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(252, 187, 127, 240), stop:0.505682 rgba(255, 222, 197, 150), stop:1 rgba(221, 246, 255, 55));"; border="border: 1px solid transparent;"; }
-
+	  
 	  QString raw = rawstyle;
 	  this->setStyleSheet( raw.replace("%1",background).replace("%2", border) );
 	}
@@ -41,11 +39,16 @@ signals:
 public:
 	LTBWidget(QWidget* parent) : QToolButton(parent){
 	  cstate = LXCB::IGNORE;
-
+		
 	  this->setPopupMode(QToolButton::InstantPopup);
 	  this->setAutoRaise(true);
 	  
-	  rawstyle = "LTBWidget{ %1 %2 border-radius: 5px;}  LTBWidget::menu-indicator{image: none;} LTBWidget::hover{ %1 border: 1px solid transparent; border-radius: 5px; } LTBWidget::menu-button{ background: transparent; width: 15px; } LTBWidget[popupMode=\"1\"]{%1 %2 border-radius: 5px; padding-right: 15px;} LTBWidget[popupMode=\"1\"]::hover{%1 border: 1px solid black; border-radius: 5px; padding-right: 15px}";
+	  rawstyle = "LTBWidget{ %1 border-radius: 5px;}  \
+	  LTBWidget::menu-indicator{image: none;} \
+	  LTBWidget::hover{ background: rgba(255, 255, 255, 30); border-radius: 5px; } \
+	  LTBWidget::menu-button{ background: transparent; width: 15px; } \
+	  LTBWidget[popupMode=\"1\"]{%1 %2 border-radius: 5px; padding-right: 15px;} \
+	  LTBWidget[popupMode=\"1\"]::hover{%1 border: 1px solid black; border-radius: 5px; padding-right: 15px}";
 	  updateBackground();
 	}
 	
