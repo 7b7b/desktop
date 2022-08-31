0741f6180003f1b37112ac2a11e4b888b4e52a92
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2022-08-31 12:46:30 +0300

    Update LSession.cpp

--- LSession.cpp
+++ LSession.cpp
@@ -180,7 +180,7 @@ void LSession::setupSession(){
   //Setup the user's lumina settings directory as necessary
     //splash.showScreen("user");
   //if(DEBUG){ qDebug() << " - Init User Files:" << timer->elapsed();}
-  //checkUserFiles(); //adds these files to the watcher as well
+  checkUserFiles();
 
   //Initialize the internal variables
   DESKTOPS.clear();
